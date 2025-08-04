43e25916055975f639ccd3456ac5130f6f6578ce
Harshit Gupta <harshit4277448@live.com>
2025-08-04 20:13:03 +0530

    fix: Update settings to retrieve SECRET_KEY from environment variable and raise error if not set

--- base/settings.py
+++ base/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=t2-v=8+n&v#cq-1(q%g7_zkiy&5-no1&np10acg^$p(v3e)an'
+SECRET_KEY = os.environ.get('TRACKER_SECRET_KEY')
+
+if not SECRET_KEY:
+    raise ValueError("The TRACKER_SECRET_KEY environment variable is not set.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
